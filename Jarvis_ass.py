3ca01b795a8e2e6a1993cf20171baaafffa76e52
Tusharmakholiya10 <makholiyatushar@gmail.com>
2026-06-11 15:51:22 +0530

    Initial Commit

---
+++ Jarvis_ass.py
@@ -0,0 +1,54 @@
+import pyttsx3
+import datetime
+import speech_recognition as sr
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice' , voices[0].id) 
+
+
+def speak(audio):
+    engine.stop()
+    # print(audio)
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >=0 and hour< 12:
+        speak("good morning")
+    elif hour>=12 and hour < 18:
+        speak("good afternoon")
+    else:
+        speak("good evening")
+    
+    speak("I am jarvis sir!, Please tell me how may i help you") 
+
+def takeCommand():
+    # It takes microphone i/p from the user adn string is the o/p
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening.....")
+        r.pause_threshold = 1  
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio , language = 'en-in')
+        print(f"User said: {query}\n")
+    except Exception as e:
+        # print(e)  it will write the error (doesn't looks good)
+
+        print("Say That again Pls....")
+        return"None"
+    return query
+
+
+
+if __name__ == "__main__":  
+    # speak("tushar is all good")
+    wishMe()
+    takeCommand()
